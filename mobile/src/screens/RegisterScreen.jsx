e75f022480b491b21257660dfcc41faed2831aab
Abdelhaq laachari <93976987+abdelhaq-laachari@users.noreply.github.com>
2023-04-02 11:39:43 +0000

    create full name input field

--- mobile/src/screens/RegisterScreen.jsx
+++ mobile/src/screens/RegisterScreen.jsx
@@ -1,11 +1,31 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TextInput, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
 const RegisterScreen = () => {
   return (
     <>
       <View style={styles.container}>
-        <Text>Register Screen</Text>
+        {/* logo */}
+        <View>
+          <Text>SwiftDelivery</Text>
+        </View>
+        {/* subtitle */}
+        <View>
+          <Text>
+            Please enter your information to create an account with
+            SwiftDelivery
+          </Text>
+        </View>
+        {/* full name input with icons */}
+        <View className="flex flex-row items-center space-x-2 bg-slate-100 w-[90%] h-[60px] rounded-full px-3 py-2 ">
+          <View className="flex items-center justify-center w-10 h-10 rounded-full bg-white border border-black">
+            <Ionicons name="person-outline" size={30} color="black" />
+          </View>
+          <View className="flex bg-reds-500 w-[80%] text-white ">
+            <TextInput className="flex h-full items-center text-base font-semibold text-black" placeholder="Full Name" />
+          </View>
+        </View>
       </View>
     </>
   );
