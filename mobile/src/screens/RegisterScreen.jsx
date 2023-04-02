cdde8c0f79c2bb6a75644a0c897700d5662c8891
Abdelhaq laachari <93976987+abdelhaq-laachari@users.noreply.github.com>
2023-04-02 13:34:28 +0000

    feat: register screen done

--- mobile/src/screens/RegisterScreen.jsx
+++ mobile/src/screens/RegisterScreen.jsx
@@ -1,43 +1,113 @@
-import React from "react";
-import { StyleSheet, Text, TextInput, View } from "react-native";
-import { Ionicons } from "@expo/vector-icons";
+import React, { useState } from "react";
+import {
+  Image,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  View,
+} from "react-native";
+import { Ionicons, MaterialIcons } from "@expo/vector-icons";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import Header from "../components/Header";
 
 const RegisterScreen = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const hidePassword = () => {
+    setShowPassword(!showPassword);
+  };
+
   return (
     <>
-      <View style={styles.container}>
-        {/* logo */}
-        <View>
-          <Text>SwiftDelivery</Text>
+      <View className="bg-white flex w-full h-full items-center pt-7">
+        <Header />
+        {/* all inputs field */}
+        <View className="flex w-full h-[37%] justify-around items-center mb-5 ">
+          {/* full name input */}
+          <View className="flex flex-row items-center space-x-2 bg-slate-100 w-[90%] h-[60px] rounded-full px-3 py-2 ">
+            <View className="flex items-center justify-center w-10 h-10 rounded-full bg-white border border-black">
+              <Ionicons name="person-outline" size={24} color="black" />
+            </View>
+            <View className="flex w-[80%] text-white ">
+              <TextInput
+                className="flex h-full items-center text-base font-semibold text-black"
+                placeholder="Full Name"
+              />
+            </View>
+          </View>
+          {/* Email input */}
+          <View className="flex flex-row items-center space-x-2 bg-slate-100 w-[90%] h-[60px] rounded-full px-3 py-2 ">
+            <View className="flex items-center justify-center w-10 h-10 rounded-full bg-white border border-black">
+              <MaterialIcons name="alternate-email" size={24} color="black" />
+            </View>
+            <View className="flex  w-[80%] text-white ">
+              <TextInput
+                className="flex h-full items-center text-base font-semibold text-black"
+                placeholder="Email"
+              />
+            </View>
+          </View>
+          {/* Password input */}
+          <View className="flex flex-row items-center space-x-2 bg-slate-100 w-[90%] h-[60px] rounded-full px-3 py-2 ">
+            <View className="flex items-center justify-center w-10 h-10 rounded-full bg-white border border-black">
+              <Ionicons name="lock-closed-outline" size={24} color="black" />
+            </View>
+            <View className="flex  w-[70%] text-white ">
+              <TextInput
+                className="flex h-full items-center text-base font-semibold text-black"
+                placeholder="Password"
+                secureTextEntry={!showPassword}
+              />
+            </View>
+            <TouchableOpacity onPress={hidePassword}>
+              <MaterialIcons
+                name={showPassword ? "visibility-off" : "visibility"}
+                size={24}
+                color="#666666"
+              />
+            </TouchableOpacity>
+          </View>
         </View>
-        {/* subtitle */}
-        <View>
-          <Text>
-            Please enter your information to create an account with
-            SwiftDelivery
+        {/* register button */}
+        <View className="flex bg-black items-center justify-center w-[60%] h-[60px] rounded-full  mt-5">
+          <TouchableOpacity>
+            <Text className="text-white font-semibold text-lg">Sign Up</Text>
+          </TouchableOpacity>
+        </View>
+        {/* login button */}
+        <View className="flex flex-row items-center space-x-1 my-5">
+          <Text className="text-base font-semibold">
+            Already have an account?
           </Text>
+          <TouchableOpacity>
+            <Text className="text-blue-600 text-base font-semibold">Login</Text>
+          </TouchableOpacity>
         </View>
-        {/* full name input with icons */}
-        <View className="flex flex-row items-center space-x-2 bg-slate-100 w-[90%] h-[60px] rounded-full px-3 py-2 ">
-          <View className="flex items-center justify-center w-10 h-10 rounded-full bg-white border border-black">
-            <Ionicons name="person-outline" size={30} color="black" />
-          </View>
-          <View className="flex bg-reds-500 w-[80%] text-white ">
-            <TextInput className="flex h-full items-center text-base font-semibold text-black" placeholder="Full Name" />
-          </View>
+        {/* make line  */}
+        <View className="flex flex-row w-[80%] items-center space-x-2 my-5">
+          <View className="flex-1 h-[1px] bg-gray-300"></View>
+          <Text className="text-base font-semibold text-slate-500">
+            Sign up with
+          </Text>
+          <View className="flex-1 h-[1px] bg-gray-300"></View>
+        </View>
+        {/* social media buttons */}
+        <View className="flex flex-row items-center justify-center space-x-5 ">
+          <TouchableOpacity className="flex items-center justify-center w-[50px] h-[50px] rounded-full bg-slate-100">
+            <Ionicons name="logo-google" size={24} color="#DB4437" />
+          </TouchableOpacity>
+          <TouchableOpacity className="flex items-center justify-center w-[50px] h-[50px] rounded-full bg-slate-100">
+            <MaterialIcons name="facebook" size={30} color="#4267B2" />
+          </TouchableOpacity>
+          <TouchableOpacity className="flex items-center justify-center w-[50px] h-[50px] rounded-full bg-slate-100">
+            <Ionicons name="logo-twitter" size={24} color="#1DA1F2" />
+          </TouchableOpacity>
         </View>
       </View>
     </>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
 
 export default RegisterScreen;
